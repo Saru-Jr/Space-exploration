6f12d6f68f86f1cef27f0eeb5862200088225c48
SARU <ndimbiarivelo.tsantatsiory@gmail.com>
2025-05-31 11:40:39 +0400

    card

---
+++ script.js
@@ -0,0 +1,17 @@
+var TrandingSlider = new Swiper('.content', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  navigation: {
+    nextEl: '.arrow.arrow-right',
+    prevEl: '.arrow.arrow-left',
+  }
+});
